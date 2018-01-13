104d7f4402554f25ce4ce00cab070e5caa898c79
Thomas De Lange <thomas.m.delange@gmail.com>
2018-01-13 18:28:33 +0100

    kleine update

--- software/NetflixStatistics/src/Clicklistener/TableEditor.java
+++ software/NetflixStatistics/src/Clicklistener/TableEditor.java
@@ -23,19 +23,16 @@ public class TableEditor {
             ResultSetMetaData rsmd = resultSet.getMetaData();
             int columnnNumber = rsmd.getColumnCount();
             while (resultSet.next()) {
-
                 Object[] objects = new Object[columnnNumber];
 
                 for (int i = 0; i < columnnNumber; i++) {
                     objects[i] = resultSet.getObject(i + 1);
                 }
-
-
                 //tableModel.addRow(objects);
 
             }
             resultTable.setModel(tableModel);
-
+            tableModel.fireTableDataChanged();
         } catch(
                 SQLException e1)
 
@@ -43,14 +40,11 @@ public class TableEditor {
             e1.printStackTrace();
         }
     }
-<<<<<<< HEAD:software/NetflixStatistics/src/Clicklistener/TableFiller.java
-
-    public void emptyTable(DefaultTableModel tableModel) {
-            tableModel.setRowCount(0);
-=======
     public void emptyTable(){
-        tableModel.setRowCount(0);
->>>>>>> 469e0405505fd56328ccf2d2064274d3c0062e64:software/NetflixStatistics/src/Clicklistener/TableEditor.java
+        tableModel.getDataVector().removeAllElements();
+        tableModel.fireTableDataChanged();
+
     }
 }
+
 
