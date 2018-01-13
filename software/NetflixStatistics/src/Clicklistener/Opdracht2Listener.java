7777f7615bf9ac730cba5e1a40c474cc6b43674d
Thomas De Lange <thomas.m.delange@gmail.com>
2018-01-13 18:08:44 +0100

    New Table Filler Class
    easy table filling

--- software/NetflixStatistics/src/Clicklistener/Opdracht2Listener.java
+++ software/NetflixStatistics/src/Clicklistener/Opdracht2Listener.java
@@ -1,38 +1,29 @@
 package Clicklistener;
 
 import applicationlogic.SqlConnection;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
-
-/*
-
-Opdracht1Listener
-
-Als er op de bijbehorende knop wordt gedrukt wordt actionPerformed()
-Regelt het afsluiten van het programma
-Zet de connectie op van de database
- */
-
-public class Opdracht1Listener implements ActionListener {
+public class Opdracht2Listener implements ActionListener {
 
     private JTable resultTable;
     private SqlConnection sqlConnection;
     private JTextArea input;
     private DefaultTableModel tableModel;
-    private TableFiller tableFiller;
 
-    public Opdracht1Listener(JTable resultTable, JTextArea input, SqlConnection sqlConnection, DefaultTableModel tableModel) {
+    public Opdracht2Listener(JTable resultTable, DefaultTableModel tableModel, JTextArea inputSerieTitel, JTextArea inputAccountNR, SqlConnection sqlConnection) {
         this.resultTable = resultTable;
         this.input = input;
         this.sqlConnection = sqlConnection;
         this.tableModel = tableModel;
-        tableFiller = new TableFiller(tableModel, resultTable);
-
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -42,9 +33,12 @@ public class Opdracht1Listener implements ActionListener {
                                                                 "ON Aflevering.AfleveringID = Bekeken.AfleveringID\n" +
                                                                 "INNER JOIN Content\n" +
                                                                 "ON Content.ContentID = Aflevering.ContentID\n" +
-                                                                "WHERE Content.Titel =" + "'" + input.getText() + "'" + "\n" +
-                                                                "GROUP BY Bekeken.AfleveringID, Aflevering.Titel");
+                                                                "INNER JOIN Account\n" +
+                                                                "ON Account.AccountNR = Bekeken.AccountNR\t\n" +
+                                                                "WHERE\tContent.Titel = 'Sherlock'\n" +
+                                                                "\t\tAND\n" +
+                                                                "\t\tAccount.AccountNR = '1215426'\n" +
+                                                                "GROUP BY Bekeken.AfleveringID, Aflevering.Titel\n");
 
-        tableFiller.fillTable(resultSet);
     }
 }
